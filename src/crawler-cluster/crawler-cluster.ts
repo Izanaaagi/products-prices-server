884e63c8b8ea2382c42f4f3bce4752528ca7a0ba
Izanaaagi <vlad.rotar2@gmail.com>
2022-02-15 20:35:02 +0200

    refactor clusters

---
+++ src/crawler-cluster/crawler-cluster.ts
@@ -0,0 +1,77 @@
+import { cpus } from 'os';
+import { Supermarket } from '../interfaces/supermarket';
+import cluster from 'cluster';
+import { StoreCrawler } from '../crawlers/store-crawler';
+import { CrawlerType } from '../enums/crawler-type';
+import { ZakazUACrawler } from '../crawlers/zakaz-ua-crawler';
+import { ShopUaCrawler } from '../crawlers/shop-ua-crawler';
+import { AtbCrawler } from '../crawlers/atb-crawler';
+import { PuppeteerOptions } from '../interfaces/puppeteer-options';
+
+export class CrawlerCluster {
+  private readonly availableCpusCount: number;
+  private supermarketIndex: number;
+  private readonly supermarkets: Array<Supermarket>;
+  private readonly supermarketsCount: number;
+  private readonly puppeteerOptions: PuppeteerOptions;
+
+  constructor(
+    supermarkets: Array<Supermarket>,
+    puppeteerOptions?: PuppeteerOptions
+  ) {
+    this.supermarkets = supermarkets;
+    this.supermarketsCount = supermarkets.length;
+    this.availableCpusCount = cpus().length - 1;
+    this.supermarketIndex = 0;
+    this.puppeteerOptions = puppeteerOptions;
+  }
+
+  async startClusters(): Promise<void> {
+    if (cluster.isMaster) {
+      while (this.supermarketIndex < this.availableCpusCount) {
+        const worker = cluster.fork();
+        worker.send(this.supermarkets[this.supermarketIndex]);
+        this.supermarketIndex++;
+      }
+
+      cluster.on('exit', (worker) => {
+        console.log(`worker ${worker.process.pid} died`);
+        if (this.supermarketIndex < this.supermarketsCount) {
+          const newWorker = cluster.fork();
+          newWorker.send(this.supermarkets[this.supermarketIndex]);
+          this.supermarketIndex++;
+        }
+      });
+    } else {
+      console.log(`worker ${process.pid} started`);
+      process.on('message', async (supermarket: Supermarket): Promise<void> => {
+        const crawler = this.crawlerGenerator(supermarket);
+        await crawler.start();
+        process.kill(process.pid);
+      });
+    }
+  }
+
+  private crawlerGenerator(supermarket: Supermarket): StoreCrawler {
+    switch (supermarket.crawlerType) {
+      case CrawlerType.ZAKAZ_UA:
+        return new ZakazUACrawler(
+          supermarket.url,
+          supermarket.storeTitle,
+          this.puppeteerOptions
+        );
+      case CrawlerType.SHOP_UA:
+        return new ShopUaCrawler(
+          supermarket.url,
+          supermarket.storeTitle,
+          this.puppeteerOptions
+        );
+      case CrawlerType.ATB:
+        return new AtbCrawler(
+          supermarket.url,
+          supermarket.storeTitle,
+          this.puppeteerOptions
+        );
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,10 @@
-import { ZakazUACrawler } from './crawlers/zakaz-ua-crawler';
-import { puppeteerOptions } from './puppeteer-options';
-import { cpus } from 'os';
-import cluster from 'cluster';
-import { StoreCrawler } from './crawlers/store-crawler';
-import { AtbCrawler } from './crawlers/atb-crawler';
-import { ShopUaCrawler } from './crawlers/shop-ua-crawler';
-import { Supermarket } from './interfaces/supermarket';
-import { CrawlerType } from './enums/crawler-type';
+import { CrawlerCluster } from './crawler-cluster/crawler-cluster';
 import { supermarkets } from './supermarkets';
-
-const generateCrawlers = (
-  supermarkets: Array<Supermarket>
-): Array<StoreCrawler> => {
-  const crawlers: Array<StoreCrawler> = [];
-  supermarkets.forEach((supermarket) =>
-    crawlers.push(crawlerGenerator(supermarket))
-  );
-  return crawlers;
-};
-
-const crawlerGenerator = (supermarket: Supermarket): StoreCrawler => {
-  switch (supermarket.crawlerType) {
-    case CrawlerType.ZAKAZ_UA:
-      return new ZakazUACrawler(
-        supermarket.url,
-        supermarket.storeTitle,
-        puppeteerOptions
-      );
-    case CrawlerType.SHOP_UA:
-      return new ShopUaCrawler(
-        supermarket.url,
-        supermarket.storeTitle,
-        puppeteerOptions
-      );
-    case CrawlerType.ATB:
-      return new AtbCrawler(
-        supermarket.url,
-        supermarket.storeTitle,
-        puppeteerOptions
-      );
-  }
-};
+import { puppeteerOptions } from './puppeteer-options';
 
 const main = async (): Promise<void> => {
-  if (cluster.isMaster) {
-    const availableCpusCount = cpus().length - 1;
-    const supermarketsCount = supermarkets.length;
-    let crawlerIndex = 0;
-
-    for (let i = 0; i < availableCpusCount; i++) {
-      const worker = cluster.fork();
-      worker.send(supermarkets[crawlerIndex]);
-      crawlerIndex++;
-    }
-
-    cluster.on('exit', (worker) => {
-      console.log(`worker ${worker.process.pid} died`);
-      if (crawlerIndex < supermarketsCount) {
-        const newWorker = cluster.fork();
-        newWorker.send(supermarkets[crawlerIndex]);
-        crawlerIndex++;
-      }
-    });
-  } else {
-    console.log(`worker ${process.pid} started`);
-    process.on('message', async (supermarket: Supermarket): Promise<void> => {
-      const crawler = crawlerGenerator(supermarket);
-      await crawler.start();
-      process.kill(process.pid);
-    });
-  }
+  const crawlerCluster = new CrawlerCluster(supermarkets, puppeteerOptions);
+  await crawlerCluster.startClusters();
 };
 
 main();
